c2fa6af5453696800b39afa72e6ec92232d05171
Adam <57573881+AdamCarter11@users.noreply.github.com>
2022-10-10 23:37:06 -0700

    Player jumping

--- 200SkateChase/Assets/Scripts/Player.cs
+++ 200SkateChase/Assets/Scripts/Player.cs
@@ -4,16 +4,57 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //Jumping variables
+    [SerializeField] float gravity;
+    [SerializeField] float maxJumpHold;
+    [SerializeField] float jumpGroundThreshold = 1;
+    [SerializeField] float jumpVelocity;
+    [SerializeField] float groundHeight;
+    private bool isGrounded;
+    private bool isHoldingJump;
+    private Vector2 velocity;
+    private float holdJumpTimer = 0;
     
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 pos = transform.position;
+        float groundDist = Mathf.Abs(pos.y-groundHeight);
+        if(isGrounded || groundDist <= jumpGroundThreshold){
+            if(Input.GetKeyDown(KeyCode.Space)){
+                isGrounded = false;
+                velocity.y = jumpVelocity;
+                isHoldingJump = true;
+                holdJumpTimer = 0;
+            }
+        }
+        if(Input.GetKeyUp(KeyCode.Space)){
+            isHoldingJump = false;
+        }
+    }
+
+    private void FixedUpdate() {
+        Vector2 pos = transform.position;
+        if(!isGrounded){
+            if(isHoldingJump){
+                holdJumpTimer += Time.fixedDeltaTime;
+                if(holdJumpTimer >= maxJumpHold){
+                    isHoldingJump = false;
+                }
+            }
+            pos.y += velocity.y * Time.fixedDeltaTime;
+            if(!isHoldingJump){
+                velocity.y += gravity * Time.fixedDeltaTime;
+            }
+            if(pos.y <= groundHeight){
+                pos.y = groundHeight;
+                isGrounded = true;
+            }
+        }
+        transform.position = pos;
     }
 }
